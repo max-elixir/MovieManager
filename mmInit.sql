6d77a6b488fe703819eec27c2b2e82abe8673d74
max-elixir <maximilianguzman@gmail.com>
2019-04-15 17:07:13 -0500

    example values to insert into database

---
+++ mmInit.sql
@@ -0,0 +1,56 @@
+insert into film values (0,'Avengers: Endgame', 3.0, 'movie', 300.00, 'superhero');
+insert into film values (1, 'Spiderman: Far From Home', 0.05, 'trailer', 50.00, 'superhero');
+insert into film values (2, 'Toy Story 4', 'trailer' 0.05, 50.00, 'Family');
+insert into film values (3, 'Hellboy (2019)', 2.0, 'movie', 200.00, 'action' )
+insert into film values (4, 'Little', 1.5, 'movie', 200.00, 'comedy');
+
+insert into trailer values(1, green);
+insert into trailer values(2, green);
+
+insert into movie(0, '26-APR-2019', '26-JUL-2019', 'pg-13');
+insert into movie(3, '12-APR-2019', '12-MAY-2019', 'r');
+insert into movie(4, '12-APR-2019', '12-MAY-2019', 'PG-13');
+
+insert into movie_schedule values(0, 0, 1, 10.00, 1, '26-APR-2019');
+insert into movie_schedule values(0, 1, 2, 10.30, 1, '26-APR-2019');
+insert into movie_schedule values(0, 2, 3, 12.00, 1, '26-APR-2019');
+insert into movie_schedule values(0, 3, 1, 2.00, 1, '26-APR-2019');
+insert into movie_schedule values(1, 4, 1, 9.57, 1, '26-APR-2019');
+insert into movie_schedule values(1, 5, 2, 10.27, 1, '26-APR-2019');
+insert into movie_schedule values(1, 6, 3, 11.57, 1, '26-APR-2019');
+insert into movie_schedule values(3, 7, 2, 10.45, 0, '26-APR-2019');
+
+insert into ad values(0, 'Coca-Cola: summer vacation', 0.01, 0, 'coca-cola', 100.50);
+insert into ad values(1, 'lets all go to the lobby', 0.01, 1, 'theater', 0.0);
+insert into ad values(2, 'SA Film Festival', 0.01, 0, 'City of San Antonio', 20.00);
+
+insert into ad_schedule values(0, 8, 9.56);
+insert into ad_schedule values(1, 9, 10.26);
+insert into ad_schedule values(1, 10, 11.26);
+
+insert into users values(0, 'max_elixir', 'blue12345', 'Max', 'Guzman', 'SA');
+insert into users values(1, 'buitrago', 'groupLeader1', 'Isaac', 'Buitrago', 'SA');
+insert into users values(2, 'jason-kha', 'yoshi', 'Jason', 'Kha', 'SA');
+insert into users values(3, 'bsmith', 'bigboss', 'Brad', 'Smith', 'O');
+insert into users values(4, 'hmendez', 'yugioh', 'Henry', 'Mendez', 'M');
+insert into users values(5, 'asolis', 'coolmom', 'Amy', 'Solis', 'M');
+insert into users values(6, 'rotiz', 'bigknife', 'Roman', 'Ortiz', 'M');
+insert into users values(7, 'pochoa', 'yeahthatsfine', 'Paul', 'Ochoa', 'M');
+insert into users values(8, 'user', 'password', 'john', 'doe', 'C');
+insert into users values(9, 'username', 'password', 'jane', 'doe', 'C');
+insert into users values(10, 'Markiplier', 'mustache', 'Mark', 'Fishbach', 'C');
+insert into users values(11, 'rslavin', 'systems', 'Rocky', 'Slavin', 'C');
+insert into users values(12, 'lclark', 'readthenotes', 'Larry', 'Clark', 'C');
+
+insert into customer_tickets(0, 0);
+insert into customer_tickets(1, 0);
+insert into customer_tickets(2, 0);
+insert into customer_tickets(3, 1);
+insert into customer_tickets(4, 1);
+insert into customer_tickets(5, 1);
+insert into customer_tickets(6, 3);
+insert into customer_tickets(7, 3);
+insert into customer_tickets(8, 3;
+insert into customer_tickets(8, 2);
+insert into customer_tickets(9, 3);
+insert into customer_tickets(10, 2);
