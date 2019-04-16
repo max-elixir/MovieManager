d7b29fc31d284f02110c36cdd6f8681998ac3d61
Isaac Buitrago <mightyking7@gmail.com>
2019-04-16 15:25:22 -0500

    retrieved movie titles with release date of two months

--- queries.sql
+++ queries.sql
@@ -3,8 +3,12 @@ select title from film as f join movies as m
 on f.filmId = m.movieId where m.rating = 'PG-13' or m.rating = 'R');
 
 -- Retrieve movie titles with a release date of 2 months --
+def current_date = select current_date from dual;
+def past = select add_months(sysdate,-2) from dual;
+
 select title from film as f join movies as m 
-on f.filmId = m.movieId where m.release_date = Date.currentTime() - 2 month time span
+on f.filmId = m.movieId where m.release_date >=
+past and m.release_date <= current_date;
 
-
+-- Retrieve commercials with similar genre and rating of movie --
 
