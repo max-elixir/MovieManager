d24f82ac1785017ae9d8c5a1a1afbc2bdafb10fa
Isaac Buitrago <mightyking7@gmail.com>
2019-04-16 15:38:07 -0500

    Added migration scripts to undo results of mm.sql

---
+++ migration.sql
@@ -0,0 +1,11 @@
+-- drop all tables --
+drop table trailer;
+drop table movies;
+drop table film;
+drop table movie_schedule;
+drop table ad_schedule;
+drop table trailers;
+drop table ad;
+drop table users;
+drop table customer_tickets;
+
