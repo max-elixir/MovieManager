affe88a511ff31a43be8c04cbe98518004a706e5
Jason Kha <zki628@my.utsa.edu>
2019-04-16 17:24:51 -0500

    Update migration.sql
    Added references comment

--- migration.sql
+++ migration.sql
@@ -1,9 +1,9 @@
 -- drop all tables --
-drop table customer_tickets;
-drop table ad_schedule;
-drop table movie_schedule;
-drop table movies;
-drop table trailer;
+drop table customer_tickets; -- references users and movie_schedule --
+drop table ad_schedule;      -- references ad and movie_schedule --
+drop table movie_schedule;   -- references on film --
+drop table movies;           -- references on film --
+drop table trailer;          -- references on film --
 drop table film;
 drop table users;
 drop table ad;
