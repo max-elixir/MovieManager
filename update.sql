e3672908b828d084932a582f5188540e4cdae573
max-elixir <maximilianguzman@gmail.com>
2019-04-16 19:11:43 -0500

    simple updates to database for testing:

--- update.sql
+++ update.sql
@@ -1,10 +1,13 @@
---1
+--1 New user to the database
+insert into users values (42, 'newuser', 'badpassword', 'no', 'name', 'C');
 
---2
+--2 a particular ad isnt used anymore 
+delete from ad where title = 'SA Film Festival';
 
---3
+--3 delete the old movies that cant be used
+delete from movies where expiration_date <  to_date(sysdate);
 
---4 The theater is keeping a particular movie for a month
+--4 The theater is keeping a particular movie for an extra month
 update movie set expiration_date+31 where movieId=0;
 
 --5 - Project #3 is broken, so cancel the movies in that theater for the day
