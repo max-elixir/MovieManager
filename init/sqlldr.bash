0904a09617b7bd8c171216ab29c7502c4c448e7d
Isaac Buitrago <mightyking7@gmail.com>
2019-04-19 17:09:44 -0500

    Script to quickly load data into the db

--- init/sqlldr.bash
+++ init/sqlldr.bash
@@ -1,10 +1,30 @@
 #!/bin/bash
+set user="hwt460"
+set pass="01592719"
 
-if [ $# -ne 1 ]; then
-        echo "Invalid amount of arguments"
-        exit
-fi
+# load films
+sqlldr control=film.ctl log=film.log bad=film.bad
 
-sqlldr control=$1.ctl log=$1.log bad=$1.bad
+# load movies
+sqlldr control=movies.ctl log=movies.log bad=movies.bad
 
-cat $1.log
+# load trailer
+sqlldr control=trailer.ctl log=trailer.log bad=trailer.bad
+
+# load trailer schedules
+sqlldr control=trailer_schedule.ctl log=trailer_schedule.log bad=trailer_schedule.bad
+
+# load movie_schedule
+sqlldr control=movie_schedule.ctl log=movie_schedule.log bad=movie_schedule.bad
+
+# load ad's
+sqlldr control=ad.ctl log=ad.log bad=ad.bad
+
+# load ad_schedule
+sqlldr control=ad_schedule.ctl log=ad_schedule.log bad=ad_schedule.bad
+
+# load users
+sqlldr control=users.ctl log=users.log bad=users.bad
+
+# load customer_tickets
+sqlldr control=customer_tickets.ctl log=customer_tickets.log bad=customer_tickets.bad
