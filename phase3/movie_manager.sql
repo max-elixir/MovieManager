845c966b0f4dd7cf3f170664266739f9d168ce83
Isaac Buitrago <mightyking7@gmail.com>
2019-05-04 21:45:18 -0500

    Defined method signature for schedule_trailer and procedure body.

--- phase3/movie_manager.sql
+++ phase3/movie_manager.sql
@@ -1,7 +1,6 @@
 create or replace package body movie_manager as
   procedure get_schedule
     (today in varchar2 default to_char(SYSDATE,'DD-MON-YYYY')) is
-
     cursor sched is 
       select f.title, f.genre, f.runtime, m.rating, ms.room_num, 
       to_char(ms.start_time, 'hh:mi AM') start_time
@@ -30,6 +29,24 @@ create or replace package body movie_manager as
       when others then
         dbms_output.put_line('Invalid date, please try again.');
   end;
-end;
+
+procedure schedule_trailer (trailer in varchar2, screenId in integer, st in varchar2) is
+  cursor pre_sched is 
+    get_pre_schedule(screenId);
+
+  trailerId integer;
+  begin
+    trailerId:= select t.filmId from trailer t, film f where t.title = trailer and f.filmtype = 'trailer';
+
+    insert into trailer_schedule (trailerId, screenId, start_time) values(
+    ,
+    (select screenId from movie_schedule ms join film f on f.filmId = ms.filmId
+    where f.title = 'Hellboy (2019)' and f.filmtype = 'movie'),
+    (select current_timestamp from dual) - 1);
+  end;
+
+function get_pre_schedule(screenId in integer)
+return 
+
 /
 show errors
--- phase3/movie_manager_spec.sql
+++ phase3/movie_manager_spec.sql
@@ -1,6 +1,9 @@
 create or replace package movie_manager as
   procedure get_schedule
     (today in varchar2 default to_char(SYSDATE,'DD-MON-YYYY'));
+
+  procedure schedule_trailer(trailer in varchar2, movie in varchar2, 
+  screenId in integer, st in varchar2);
 end;
 /
 show errors
